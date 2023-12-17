8dbbcb1fba857da4627d09fce1f5f944ab37d2bb
MOBOLLNE <1525996738@qq.com>
2023-12-17 10:46:59 +0900

    Basically finished the main funtion. It could be used as an application!(after deploying the web service on the EC2)

--- backend.py
+++ backend.py
@@ -1,12 +1,19 @@
 import boto3
-import json
 from boto3 import resource
 from boto3.dynamodb.conditions import Key, Attr
 from flask import Flask, jsonify, request
-from datetime import datetime
+from flask_jwt_extended import jwt_required, JWTManager, get_jwt_identity, create_access_token
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = '17b9Junp4u9ESG_yfioo-TyqvjK2qO4='
+jwt = JWTManager(app)
 
 dynamodb = boto3.resource('dynamodb')
 table = resource('dynamodb').Table('textbase')
+authority_table = resource('dynamodb').Table('authority_table')
+
+
 def insert(username,text):
     global requ_time
     requ_time = datetime.now().isoformat()
@@ -21,6 +28,7 @@ def insert(username,text):
 
 def new_check(username):
     global requ_time
+    print(requ_time)
     response = table.scan(
         FilterExpression=Key('user_id').eq(username) & Attr('created_date').gt(requ_time)
     )
@@ -75,36 +83,73 @@ def status_switch(username,text):
         )
 
 
-app = Flask(__name__)
 
+@app.route('/register', methods=["POST"])
+def register():
+    email = request.form.get('email')
+    response = authority_table.scan(
+        FilterExpression=Key('user_id').eq(email)
+    )
+    items = response['Items']
+    if items:
+        return 'All ready registered', 500
 
+    password = request.form.get('password')
+    authority_table.put_item(
+        Item={
+            'email': email,
+            'key': password,
+        }
+    )
+    access_token = create_access_token(identity=email, expires_delta=timedelta(days=180))
+    return jsonify(access_token=access_token)
+@app.route('/login', methods=["POST"])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    response = authority_table.scan(
+        FilterExpression=Key('email').eq(email) & Attr('key').eq(password)
+    )
+    items = response['Items']
+    if items:
+        access_token = create_access_token(identity=email,expires_delta=timedelta(days=180))
+        return jsonify(access_token=access_token)
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
 
-# @app.route('/login', methods=['GET'])
-# def log_in():
-#     item_id = request.args.get('id')
-#     response = table.get_item(Key={'id': item_id})
-#     return jsonify(response['Item'])
 
 @app.route('/get', methods=['GET'])
+@jwt_required()
 def get_text():
     username = request.args.get('id')
     databack = new_check(username)
-    return databack
+    if databack:
+        feedback = jsonify(databack)
+    else:
+        feedback = {'status': 'success'}
+    return feedback
 
 @app.route('/update', methods=['POST'])
+@jwt_required()
 def upload_text():
     data = request.form
     insert(data['id'],data['text'])
     databack = new_check(data['id'])
-    return databack
+    if databack:
+        feedback = jsonify(databack)
+    else:
+        feedback = {'status': 'success'}
+    return feedback
 
 @app.route('/switch', methods=['POST'])
+@jwt_required()
 def switch_status():
     data = request.form
     status_switch(data['id'],data['text'])
     return jsonify({'status': 'success'})
 
 @app.route('/end', methods=['GET'])
+@jwt_required()
 def delete_temp():
     username = request.args.get('id')
     temp_check(username=username)
--- main.py
+++ main.py
@@ -4,127 +4,246 @@ import requests
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import base64
+import os
+import json
 
-url = 'url'
-username = 'what'
+url = ''
 is_expansion = True
-key = b'3333333333333'
-aes = AES.new(key,AES.MODE_ECB)
-
 
 
 def main(page: ft.Page):
-    page.title = "ClipBroad"
-    page.window_always_on_top = True
-    page.window_title_bar_hidden = True
-    page.window_opacity = 0.7
-    page.window_width = 300
-    page.window_height = 68
-    page.window_left = 1500
-    page.window_top = 150
+    def to_encode(text):
+        msg = pad(text.encode('utf-8'), AES.block_size)
+        en_msg = aes.encrypt(msg)
+        en_text = base64.b64encode(en_msg).decode('utf-8')
+        return en_text
 
+    def to_decode(en_text):
+        base64_de_text = base64.b64decode(en_text)  # Base64解码
+        de_text = aes.decrypt(base64_de_text)
+        text = unpad(de_text, AES.block_size).decode('utf-8')
+        return text
 
-    def expansion(e):
-        global is_expansion
-        if is_expansion:
-            page.window_height = 500
+    def verity_page():
+        def submit(e):
+            global aes
+            key = os.urandom(32)
+            aes = AES.new(key, AES.MODE_ECB)
+
+            global username
+            username = to_encode(email.value)
+            user_pass = to_encode(password.value)
+            data = {'email': username, 'password': user_pass}
+            response = requests.post(url + '/' + func, data=data)
+            print(response)
+            if response == 401:
+                page.add(ft.Text('Wrong password or Account.', color=ft.colors.RED_400))
+            elif response == 500:
+                page.add(ft.Text('You already have an account.', color=ft.colors.RED_400))
+            elif response:
+                page.add(ft.Text('Succeed.', color=ft.colors.BLUE))
+                global access_token
+                access_token = response.json()['access_token']
+                content = {'words': access_token, 'for': username}
+                data = json.dumps(content)
+                with open(".info/ts.json", 'w') as file:
+                    file.write(data)
+                with open(".info/e.bin", 'wb') as file:
+                    file.write(key)
+                page.go('/main')
+
+        def routing_register(e):
+            global func
+            func = 'register'
+            t.value = func
             page.update()
-            is_expansion = False
-        else:
-            page.window_height = 68
+
+        def routing_login(e):
+            global func
+            func = 'login'
+            t.value = func
             page.update()
-            is_expansion = True
 
+        global func
+        func = 'register'
 
-    def copy_text(e):
-        pyperclip.copy(e.control.text)
+        page.window_width = 300
+        page.window_height = 500
 
-    def fixed(e):
-        msg = pad(new_text.value.encode('utf-8'), AES.block_size)
-        en_text = aes.encrypt(msg)
-        base64_en_text = base64.b64encode(en_text).decode('utf-8')
-        data = {'id': username, 'text': base64_en_text}
-        requests.post(url + '/switch', data=data)
-        if e.control.bgcolor == "blue":
-            e.control.bgcolor = "white"
-        else:
-            e.control.bgcolor = "blue"
-        e.control.update()
+        email = ft.TextField(keyboard_type=ft.KeyboardType.EMAIL, label="This would be your login ID",
+                             hint_text="email")
+        password = ft.TextField(label="Password", password=True, can_reveal_password=True)
 
-    def loading():
-        params = {'id': username}
-        response = requests.get(url+'/get', params=params)
-        texts = response.json()
-        if texts:
-            for t in texts:
-                base64_de_text = base64.b64decode(t['content'])  # Base64解码
-                de_text = aes.decrypt(base64_de_text)
-                msg = unpad(de_text, AES.block_size).decode('utf-8')
-                page.insert(2,
-                    ft.ElevatedButton(text=msg, bgcolor='blue', color='white',on_click=copy_text, on_long_press=fixed)
-                )
-        page.update()
-    def refresh(e):
-        params = {'id': username}
-        response = requests.get(url+'/get', params=params)
-        texts = response.json()
-        if texts:
-            for t in texts:
-                base64_de_text = base64.b64decode(t['content'])  # Base64解码
-                de_text = aes.decrypt(base64_de_text)
-                msg = unpad(de_text, AES.block_size).decode('utf-8')
-                page.insert(2,
-                    ft.ElevatedButton(text=msg, bgcolor='white', on_click=copy_text, on_long_press=fixed)
-                )
-        page.update()
+        t = ft.Text(func)
+        controls = ft.View('/verify', [
+            ft.ElevatedButton('Sign in', on_click=routing_register),
+            ft.ElevatedButton('Log in', on_click=routing_login),
+            t,
+            email,
+            password,
+            ft.ElevatedButton('Submit', on_click=submit),
+        ])
+        page.views.append(controls)
 
+    def home_page():
+        # Function of GUI elements performance
+        def expansion(e):
+            global is_expansion
+            if is_expansion:
+                page.window_height = 500
+                page.update()
+                is_expansion = False
+            else:
+                page.window_height = 68
+                page.update()
+                is_expansion = True
 
-    page.add(
-        ft.Row(
+        def copy_text(e):
+            pyperclip.copy(e.control.text)
+
+        def fixed(e):
+            en_text = to_encode(e.control.text)
+
+            data = {'id': username, 'text': en_text}
+            headers = {'Authorization': f'Bearer {access_token}'}
+            requests.post(url + '/switch', data=data, headers=headers)
+
+            if e.control.bgcolor == "blue":
+                e.control.bgcolor = "white"
+            else:
+                e.control.bgcolor = "blue"
+            e.control.update()
+
+        def open_dlg():
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
+
+        # Function for text manage with API
+        def loading():
+            params = {'id': username}
+            headers = {'Authorization': f'Bearer {access_token}'}
+            response = requests.get(url + '/get', params=params, headers=headers)
+            if response.status_code == 200:
+                texts = response.json()
+                print(texts)
+                if texts == {'status': 'success'}:
+                    pass
+                else:
+                    if not isinstance(texts, list):
+                        texts = [texts]
+                        print(texts)
+                    for t in texts:
+                        text = to_decode(t['content'])
+                        page.insert(2,
+                                    ft.ElevatedButton(text=text, bgcolor='blue', color='white', on_click=copy_text,
+                                                      on_long_press=fixed)
+                                    )
+            else:
+                open_dlg()
+
+            page.update()
+
+        def refresh(e):
+            params = {'id': username}
+            headers = {'Authorization': f'Bearer {access_token}'}
+            response = requests.get(url + '/get', params=params, headers=headers)
+            if response.status_code == 200:
+                texts = response.json()
+                if texts == {'status': 'success'}:
+                    pass
+                else:
+                    if not isinstance(texts, list):
+                        texts = [texts]
+                    for t in texts:
+                        text = to_decode(t['content'])
+                        page.insert(2,
+                                    ft.ElevatedButton(text=text, bgcolor='white', on_click=copy_text,
+                                                      on_long_press=fixed)
+                                    )
+            else:
+                open_dlg()
+
+            page.update()
+
+        def add_text(e):
+            text = ft.ElevatedButton(text=new_text.value, bgcolor='white', on_click=copy_text, on_long_press=fixed)
+            page.controls.insert(2, text)
+
+            en_text = to_encode(new_text.value)
+            data = {'id': username, 'text': en_text}
+
+            new_text.value = ''
+
+            headers = {'Authorization': f'Bearer {access_token}'}
+            response = requests.post(url + '/update', data=data, headers=headers)
+            if response.status_code == 200:
+                texts = response.json()
+                if texts == {'status': 'success'}:
+                    pass
+                else:
+                    if not isinstance(texts, list):
+                        texts = [texts]
+                    for t in texts:
+                        text = to_decode(t['content'])
+                        page.insert(2,
+                                    ft.ElevatedButton(text=text, bgcolor='white', on_click=copy_text,
+                                                      on_long_press=fixed)
+                                    )
+            else:
+                open_dlg()
+            page.update()
+
+        page.window_always_on_top = True
+        page.window_title_bar_hidden = True
+        page.window_opacity = 0.7
+        page.window_width = 300
+        page.window_height = 68
+        page.window_left = 1500
+        page.window_top = 150
+
+        dlg = ft.AlertDialog(title=ft.Text("Here is some problem;("))
+        new_text = ft.TextField(hint_text="Copy in here", on_submit=add_text, multiline=True, shift_enter=True)
+        page.add(ft.Row(
             [
                 ft.WindowDragArea(
                     ft.Container(ft.Text("CLIP-BROAD"),
                                  bgcolor=ft.colors.WHITE10, padding=3, on_click=expansion), expand=True),
                 ft.IconButton(ft.icons.REFRESH, on_click=refresh),
-                ft.IconButton(ft.icons.CLOSE, on_click=lambda _: page.window_close(),),
+                ft.IconButton(ft.icons.CLOSE, on_click=lambda _: page.window_close(), ),
 
-            ]
-        )
-    )
+            ]), new_text)
+        page.update()
+        loading()
 
-
-    def add_text(e):
-        text = ft.ElevatedButton(text=new_text.value, bgcolor='white', on_click=copy_text, on_long_press=fixed)
-        page.controls.insert(2, text)
-
-        msg = pad(new_text.value.encode('utf-8'), AES.block_size)
-        en_text = aes.encrypt(msg)
-        base64_en_text = base64.b64encode(en_text).decode('utf-8')
-        data = {'id': username, 'text': base64_en_text}
-        response = requests.post(url+'/update', data=data)
-        texts = response.json()
-        if texts:
-            for t in texts:
-                base64_de_text = base64.b64decode(t['content'])  # Base64解码
-                de_text = aes.decrypt(base64_de_text)
-                msg = unpad(de_text, AES.block_size).decode('utf-8')
-                page.insert(2,
-                    ft.ElevatedButton(text=msg, bgcolor='white', on_click=copy_text, on_long_press=fixed)
-                )
+    def route_change(handler):
+        route = ft.TemplateRoute(handler.route)
+        if route.match("/verify"):
+            verity_page()
+        elif route.match("/"):
+            home_page()
         page.update()
 
-    # def login_view(page: ft.Page, route: str):
-    #     page.add(ElevatedButton("Go to Page 2", on_click=goto_page2))
-    #
-    # def content_view(page: ft.Page, route: str):
-    #     page.add(ElevatedButton("Go to Page 2", on_click=goto_page2))
-    #
-    # page.add_route("/login", login_view)
-    # page.add_route("/page1", content_view)
+    page.on_route_change = route_change
+    page.title = "ClipBroad"
+    if not os.path.exists(".info/ts.json") or not os.path.exists(".info/e.bin"):
+        print('what')
+        page.go('/verify')
+    else:
+        with open(".info/ts.json", 'r') as file:
+            data = file.read()
+        user_info = json.loads(data)
 
-    new_text = ft.TextField(hint_text="Copy in here",on_submit=add_text, multiline=True, shift_enter=True)
-    page.add(new_text)
-    loading()
+        global username
+        global access_token
+        access_token = user_info['words']
+        username = user_info['for']
+
+        with open(".info/e.bin", 'rb') as file:
+            key = file.read()
+        global aes
+        aes = AES.new(key, AES.MODE_ECB)
+        page.go('/')
 
 
 if __name__ == "__main__":
@@ -132,6 +251,5 @@ if __name__ == "__main__":
         ft.app(target=main)
     finally:
         params = {'id': username}
-        response = requests.get(url + '/end', params=params)
-
-
+        headers = {'Authorization': f'Bearer {access_token}'}
+        response = requests.get(url + '/end', params=params, headers=headers)
